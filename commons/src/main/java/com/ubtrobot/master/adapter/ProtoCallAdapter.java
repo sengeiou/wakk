6a9bdb0832caec35542a13def27792cf969f34a6
Zhu Fang <column.fang@ubtrobot.com>
2018-04-03 20:42:51 +0800

    feat: Master Call ProtoParam 适配普通对象
    在 CallAdapter 的基础上，进一步将 ProtoPram 适配为
    POJO，并且补充同步调用的适配
    
    Change-Id: Ic4d67249f59a839926127fa6762a91673ac1890c

---
+++ commons/src/main/java/com/ubtrobot/master/adapter/ProtoCallAdapter.java
@@ -0,0 +1,98 @@
+package com.ubtrobot.master.adapter;
+
+import android.os.Handler;
+
+import com.google.protobuf.Message;
+import com.ubtrobot.async.Promise;
+import com.ubtrobot.master.call.ConvenientCallable;
+import com.ubtrobot.master.param.ProtoParam;
+import com.ubtrobot.master.transport.message.CallGlobalCode;
+import com.ubtrobot.transport.message.CallException;
+import com.ubtrobot.transport.message.Param;
+import com.ubtrobot.transport.message.Response;
+
+public class ProtoCallAdapter {
+
+    private CallAdapter mCallAdapter;
+
+    public ProtoCallAdapter(ConvenientCallable callable, Handler handler) {
+        mCallAdapter = new CallAdapter(callable, handler);
+    }
+
+    public void syncCall(String path) throws CallException {
+        syncCall(path, (Message) null);
+    }
+
+    public void syncCall(String path, Message protoParam) throws CallException {
+        mCallAdapter.callable().call(
+                path, protoParam == null ? null : ProtoParam.create(protoParam));
+    }
+
+    public <T extends Message> T
+    syncCall(String path, Class<T> resParamClass) throws CallException {
+        return syncCall(path, resParamClass);
+    }
+
+    public <T extends Message> T
+    syncCall(String path, Message protoParam, Class<T> resParamClass) throws CallException {
+        Response response = mCallAdapter.callable().call(
+                path, protoParam == null ? null : ProtoParam.create(protoParam));
+
+        try {
+            return ProtoParam.from(response.getParam(), resParamClass).getProtoMessage();
+        } catch (ProtoParam.InvalidProtoParamException e) {
+            throw new CallException(CallGlobalCode.INTERNAL_ERROR,
+                    "Response illegal protobuf message.", e);
+        }
+    }
+
+    public <D, M extends Message, F extends Exception> Promise<D, F, Void>
+    call(String path, DFProtoConverter<D, M, F> converter) {
+        return call(path, null, converter);
+    }
+
+    public <D, M extends Message, F extends Exception> Promise<D, F, Void>
+    call(String path, Message protoParam, final DFProtoConverter<D, M, F> converter) {
+        return mCallAdapter.call(
+                path,
+                protoParam == null ? null : ProtoParam.create(protoParam),
+                new CallAdapter.DFConverter<D, F>() {
+                    @Override
+                    public D convertDone(Param param) throws F {
+                        try {
+                            return converter.convertDone(ProtoParam.from(
+                                    param, converter.protoClass()).getProtoMessage());
+                        } catch (ProtoParam.InvalidProtoParamException e) {
+                            throw convertFail(new CallException(CallGlobalCode.INTERNAL_ERROR,
+                                    "Response illegal protobuf message.", e));
+                        }
+                    }
+
+                    @Override
+                    public F convertFail(CallException e) {
+                        return converter.convertFail(e);
+                    }
+                }
+        );
+    }
+
+    public <F extends Exception> Promise<Void, F, Void>
+    call(String path, Message protoParam, CallAdapter.FConverter<F> converter) {
+        return mCallAdapter.call(path, protoParam == null ? null : ProtoParam.create(protoParam),
+                converter);
+    }
+
+    public <F extends Exception> Promise<Void, F, Void>
+    call(String path, CallAdapter.FConverter<F> converter) {
+        return mCallAdapter.call(path, null, converter);
+    }
+
+    public interface DFProtoConverter<D, M extends Message, F extends Exception> {
+
+        Class<M> protoClass();
+
+        D convertDone(M protoParam);
+
+        F convertFail(CallException e);
+    }
+}

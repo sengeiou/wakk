776371a7b37e7bc0700189f42396f67045bb08b0
Zhu Fang <column.fang@ubtrobot.com>
2018-04-10 10:08:21 +0800

    feat: FirmwareDownloadService 提供抽象实现
    - 实现获取下载中的固件包组
    - 维护下载状态，实现下载控制
    - 预留实际的下载控制方法供子类实现
    
    Change-Id: Ie76fe7099ad850339095546ee49dfc58cba0a636

--- upgrade/src/main/java/com/ubtrobot/upgrade/sal/AbstractFirmwareDownloadService.java
+++ upgrade/src/main/java/com/ubtrobot/upgrade/sal/AbstractFirmwareDownloadService.java
@@ -3,46 +3,150 @@ package com.ubtrobot.upgrade.sal;
 import android.os.Handler;
 import android.os.Looper;
 
+import com.ubtrobot.async.Deferred;
 import com.ubtrobot.async.Promise;
+import com.ubtrobot.cache.CachedField;
 import com.ubtrobot.upgrade.AbstractFirmwareDownloader;
 import com.ubtrobot.upgrade.DownloadException;
 import com.ubtrobot.upgrade.FirmwarePackageGroup;
-import com.ubtrobot.upgrade.sal.FirmwareDownloadService;
 
-public class AbstractFirmwareDownloadService extends AbstractFirmwareDownloader
+public abstract class AbstractFirmwareDownloadService extends AbstractFirmwareDownloader
         implements FirmwareDownloadService {
+
+    private final CachedField<FirmwarePackageGroup> mPackageGroup;
 
     protected AbstractFirmwareDownloadService() {
         super(new Handler(Looper.getMainLooper()));
-    }
+        mPackageGroup = new CachedField<>(new CachedField.FieldGetter<FirmwarePackageGroup>() {
+            @Override
+            public FirmwarePackageGroup get() {
+                FirmwarePackageGroup packageGroup = doGetPackageGroup();
+                if (packageGroup == null) {
+                    packageGroup = new FirmwarePackageGroup.Builder("").build();
+                }
 
-    @Override
-    public boolean downloadFor(FirmwarePackageGroup packageGroup) {
-        return false;
+                return packageGroup;
+            }
+        });
+
+        if (mPackageGroup.get().getPackageCount() == 0) {
+            setState(STATE_IDLE);
+        } else {
+            setState(STATE_READY);
+        }
     }
 
     @Override
     public Promise<Void, DownloadException, Void> ready(FirmwarePackageGroup packageGroup) {
-        return null;
+        if (packageGroup.getPackageCount() == 0) {
+            throw new IllegalArgumentException("Illegal package group which has no firmware package.");
+        }
+
+        Deferred<Void, DownloadException, Void> deferred = new Deferred<>(getHandler());
+        synchronized (this) {
+            if (isIdle()) {
+                doReady(packageGroup);
+                mPackageGroup.clear();
+
+                deferred.resolve(null);
+                notifyStateChange(STATE_READY, null);
+            } else {
+                operationInIllegalState(deferred);
+            }
+        }
+
+        return deferred.promise();
     }
+
+    private void operationInIllegalState(Deferred<Void, DownloadException, Void> deferred) {
+        deferred.reject(new DownloadException.Factory().internalError("")); // TODO
+    }
+
+    protected abstract void doReady(FirmwarePackageGroup packageGroup);
 
     @Override
     public FirmwarePackageGroup packageGroup() {
-        return null;
+        synchronized (this) {
+            return mPackageGroup.get();
+        }
     }
+
+    protected abstract FirmwarePackageGroup doGetPackageGroup();
 
     @Override
     public Promise<Void, DownloadException, Void> start() {
-        return null;
+        Deferred<Void, DownloadException, Void> deferred = new Deferred<>(getHandler());
+
+        synchronized (this) {
+            if (isDownloading()) {
+                deferred.resolve(null);
+            } else if (isReady() || isPausing() || isError()) {
+                doStart();
+
+                deferred.resolve(null);
+                notifyStateChange(STATE_DOWNLOADING, null);
+            } else {
+                operationInIllegalState(deferred);
+            }
+        }
+
+        return deferred.promise();
     }
+
+    protected abstract void doStart();
 
     @Override
     public Promise<Void, DownloadException, Void> pause() {
-        return null;
+        Deferred<Void, DownloadException, Void> deferred = new Deferred<>(getHandler());
+
+        synchronized (this) {
+            if (isPausing()) {
+                deferred.resolve(null);
+            } else if (isDownloading()) {
+                doPause();
+
+                deferred.resolve(null);
+                notifyStateChange(STATE_PAUSING, null);
+            } else {
+                operationInIllegalState(deferred);
+            }
+        }
+
+        return deferred.promise();
     }
+
+    protected abstract void doPause();
 
     @Override
     public Promise<Void, DownloadException, Void> clear() {
-        return null;
+        Deferred<Void, DownloadException, Void> deferred = new Deferred<>(getHandler());
+
+        synchronized (this) {
+            if (isIdle()) {
+                deferred.resolve(null);
+                return deferred.promise();
+            }
+
+            if (isDownloading()) {
+                doPause();
+            }
+            doClear();
+            mPackageGroup.clear();
+
+            deferred.resolve(null);
+            notifyStateChange(STATE_IDLE, null);
+        }
+
+        return deferred.promise();
+    }
+
+    protected abstract void doClear();
+
+    protected void notifyError(DownloadException e) {
+        if (e == null) {
+            throw new IllegalArgumentException("Argument e is null.");
+        }
+
+        notifyStateChange(STATE_ERROR, e);
     }
 }

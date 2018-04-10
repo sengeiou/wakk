8b3e38a3dd4bc54e8995736fbb19266e487f5b19
Zhu Fang <column.fang@ubtrobot.com>
2018-04-10 17:47:35 +0800

    feat: 验证固件包任务抽象实现
    提供子类验证包 md5 的方法
    
    Change-Id: I3d41b4878d49fbc8a78da86a4f561691ffcf9284

---
+++ service-impl/upgrade/src/main/java/com/ubtrobot/upgrade/sal/AbstractVerifyingPackageTask.java
@@ -0,0 +1,69 @@
+package com.ubtrobot.upgrade.sal;
+
+import android.text.TextUtils;
+
+import com.ubtrobot.async.AsyncTask;
+import com.ubtrobot.commons.Md5Utils;
+import com.ubtrobot.ulog.FwLoggerFactory;
+import com.ubtrobot.ulog.Logger;
+import com.ubtrobot.upgrade.FirmwarePackage;
+import com.ubtrobot.upgrade.UpgradeException;
+
+import java.io.IOException;
+
+public abstract class AbstractVerifyingPackageTask extends
+        AsyncTask<Void, UpgradeException, Void> {
+
+    private static final Logger LOGGER = FwLoggerFactory.getLogger("AbstractVerifyingPackageTask");
+
+    private final FirmwarePackage mFirmwarePackage;
+
+    public AbstractVerifyingPackageTask(FirmwarePackage firmwarePackage) {
+        if (firmwarePackage == null || TextUtils.isEmpty(firmwarePackage.getLocalFile())) {
+            throw new IllegalArgumentException("Illegal firmware package. null or has no local file.");
+        }
+
+        if (firmwarePackage.isIncremental()) {
+            if (firmwarePackage.getIncrementMd5() == null
+                    || firmwarePackage.getIncrementMd5().length() != 32) {
+                throw new IllegalArgumentException("Illegal firmware package. Illegal increment md5.");
+            }
+        } else {
+            if (firmwarePackage.getPackageMd5() == null
+                    || firmwarePackage.getPackageMd5().length() != 32) {
+                throw new IllegalArgumentException("Illegal firmware package. Illegal package md5.");
+            }
+        }
+
+        mFirmwarePackage = firmwarePackage;
+    }
+
+    protected FirmwarePackage firmwarePackage() {
+        return mFirmwarePackage;
+    }
+
+    protected void verifyPackageMd5() throws UpgradeException {
+        try {
+            String md5 = Md5Utils.calculateFileMd5(mFirmwarePackage.getLocalFile());
+            if (mFirmwarePackage.isIncremental()) {
+                if (!md5.equalsIgnoreCase(mFirmwarePackage.getIncrementMd5())) {
+                    throw new UpgradeException.Factory().verifyPackageError("Verify increment " +
+                            "package md5 error. expected=" + mFirmwarePackage.getIncrementMd5() +
+                            ", actual=" + md5);
+                }
+            } else {
+                if (!md5.equalsIgnoreCase(mFirmwarePackage.getPackageMd5())) {
+                    throw new UpgradeException.Factory().verifyPackageError("Verify package md5 " +
+                            "error. expected=" + mFirmwarePackage.getIncrementMd5() + ", actual=" +
+                            md5);
+                }
+            }
+
+        } catch (IOException e) {
+            UpgradeException ue = new UpgradeException.Factory().internalError("Verify " +
+                    "package md5 failed due to package file error or system permission problem.", e);
+            LOGGER.e(ue);
+            throw ue;
+        }
+    }
+}
--- upgrade/src/main/java/com/ubtrobot/upgrade/UpgradeException.java
+++ upgrade/src/main/java/com/ubtrobot/upgrade/UpgradeException.java
@@ -6,8 +6,14 @@ import com.ubtrobot.exception.AccessServiceException;
 
 public class UpgradeException extends AccessServiceException {
 
+    private static final int CODE_VERIFY_PACKAGE_ERROR = 100000;
+
     protected UpgradeException(int code, String message, Bundle detail, Throwable cause) {
         super(code, message, detail, cause);
+    }
+
+    public boolean causedByVerifyingPackageError() {
+        return getCode() == CODE_VERIFY_PACKAGE_ERROR;
     }
 
     public static class Factory extends AccessServiceException.Factory<UpgradeException> {
@@ -16,6 +22,10 @@ public class UpgradeException extends AccessServiceException {
         protected UpgradeException
         createException(int code, String message, Bundle detail, Throwable cause) {
             return new UpgradeException(code, message, detail, cause);
+        }
+
+        public UpgradeException verifyPackageError(String message) {
+            return createException(CODE_VERIFY_PACKAGE_ERROR, message);
         }
     }
 }

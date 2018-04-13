7d027241a422c68724c11c8d7825ed3237b037dc
jing <jing.chen@ubtrobot.com>
2018-04-13 13:59:47 +0800

    fix: 反射无法获取到事件上报类
    Change-Id: I99713f24dffd6996103566d1d4014245bb18cd56

--- analytics/src/main/java/com/ubtrobot/analytics/ipc/AnalyticsConstants.java
+++ analytics/src/main/java/com/ubtrobot/analytics/ipc/AnalyticsConstants.java
@@ -2,7 +2,7 @@ package com.ubtrobot.analytics.ipc;
 
 public class AnalyticsConstants {
 
-    public static final String DELEGATE_ANALYTICS_SERVICE_NAME = "com.ubtrobot.analytics.DelegateAnalyticsService";
+    public static final String DELEGATE_ANALYTICS_SERVICE_NAME = "com.ubtrobot.analytics.sal.impl.PlatformAnalyticsService";
     public static final String PROVIDER_URI = "content://com.ubtrobot.analytics.ipc.provider.AnalyticsProvider";
 
     public static final String KEY_PROVIDER_PONG = "pong";

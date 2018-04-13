8dfb45c9f78eaa88e27f7182faa0d35254e706e2
Zhu Fang <column.fang@ubtrobot.com>
2018-04-13 11:21:55 +0800

    refact: 从访问服务异常中去除打断等原因
    Change-Id: Iac8cf5c54867e8e9123a132d0dff4f03d325c2c9

--- commons/src/main/java/com/ubtrobot/exception/AccessServiceException.java
+++ commons/src/main/java/com/ubtrobot/exception/AccessServiceException.java
@@ -2,15 +2,12 @@ package com.ubtrobot.exception;
 
 import android.os.Bundle;
 
-import com.ubtrobot.master.competition.ActivateException;
 import com.ubtrobot.master.transport.message.CallGlobalCode;
 import com.ubtrobot.transport.message.CallException;
 
 public class AccessServiceException extends RichException {
 
     private static final int CODE_FORBIDDEN = CallGlobalCode.FORBIDDEN;
-    private static final int CODE_INTERRUPTED = 420; // TODO 待 Master 更新 SDK 后，使用常量
-    private static final int CODE_OCCUPIED = 422;
     private static final int CODE_INTERNAL_ERROR = CallGlobalCode.INTERNAL_ERROR;
 
     protected AccessServiceException(int code, String message, Bundle detail, Throwable cause) {
@@ -19,14 +16,6 @@ public class AccessServiceException extends RichException {
 
     public boolean causedByForbidden() {
         return getCode() == CODE_FORBIDDEN;
-    }
-
-    public boolean causedByInterrupted() {
-        return getCode() == CODE_INTERRUPTED;
-    }
-
-    public boolean causedByOccupied() {
-        return getCode() == CODE_OCCUPIED;
     }
 
     public boolean causedByInternalError() {
@@ -43,19 +32,6 @@ public class AccessServiceException extends RichException {
 
         public E forbidden(String message, Bundle detail) {
             return createException(CODE_FORBIDDEN, message, detail, null);
-        }
-
-        public E interrupted(String message, Bundle detail) {
-            return createException(CODE_INTERRUPTED, message, detail, null);
-        }
-
-        public E occupied(String message, Bundle detail) {
-            return createException(CODE_OCCUPIED, message, detail, null);
-        }
-
-        public E occupied(ActivateException e) {
-            // TODO 待 Master 更新后，从 ActivateException 中获取相关信息
-            return createException(CODE_OCCUPIED, "The competing item is occupied.", null, e);
         }
 
         public E internalError(String message) {
